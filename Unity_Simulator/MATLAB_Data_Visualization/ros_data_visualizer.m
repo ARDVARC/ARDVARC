8bada42c2bd012d3bb0e6d598d8a0b9b01c01b1b
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2023-10-17 00:33:02 -0600

    Add RGVs to MATLAB data visualizer

--- Unity_Simulator/MATLAB_Data_Visualization/ros_data_visualizer.m
+++ Unity_Simulator/MATLAB_Data_Visualization/ros_data_visualizer.m
@@ -7,9 +7,13 @@ if (ros.internal.Global.isNodeActive == 0)
 end
 
 uas_state_sub = rossubscriber("uas_state");
+rgv1_state_sub = rossubscriber("rgv1_state");
+rgv2_state_sub = rossubscriber("rgv2_state");
 figure
 plot3(0,0,0);
 uas_path = animatedline(Color='r',MaximumNumPoints=1000);
+rgv1_path = animatedline(Color='b',MaximumNumPoints=1000);
+rgv2_path = animatedline(Color='g',MaximumNumPoints=1000);
 
 hold on
 grid minor
@@ -18,9 +22,19 @@ xlim([-25 25])
 ylim([-25 25])
 zlim([0 15])
 while (true)
-    [msg,status,statustext] = receive(uas_state_sub,1);
+    [msg,status,~] = receive(uas_state_sub,1);
     if (status == 1)
         addpoints(uas_path, msg.Pos.X, msg.Pos.Z, msg.Pos.Y);
+        drawnow
+    end
+    [msg,status,~] = receive(rgv1_state_sub,1);
+    if (status == 1)
+        addpoints(rgv1_path, msg.Pos.X, msg.Pos.Z, msg.Pos.Y);
+        drawnow
+    end
+    [msg,status,~] = receive(rgv2_state_sub,1);
+    if (status == 1)
+        addpoints(rgv2_path, msg.Pos.X, msg.Pos.Z, msg.Pos.Y);
         drawnow
     end
 end
