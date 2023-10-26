96df92e3b7a68652f24136f43c71a15cdb650a0e
ZacharyZerr <zaze6874@colorado.edu>
2023-10-26 07:34:27 -0600

    Update ardvarc_samv.m

--- Bluetooth Sim/ardvarc_samv.m
+++ Bluetooth Sim/ardvarc_samv.m
@@ -3,39 +3,42 @@ function [Az, El] = ardvarc_samv(steering_vector, iq_samples)
 %         SAMV = Sparse Asymptotic Minimum Variance
 %         ML = Maximum Likelihood
 %% Objective
-%
+%         Use the samv method to converge on a power covariance matrix,
+%         from which azmith and elevation can be extracted
 %% Inputs
 %         Steering matrix = Each column is a steering vector : a(theta)
 %         iq_samples = Complex numbers of wave form reading
 %% Outputs
 %         Az = Azmith of target
 %         El = Elevatino of target
-%% Additional Variables
-%         sigma = Noise estimate
-%         gamma = 
-%         error = 
-%% Initialize V
+%% Initialize Variables
     x = iq_samples;
     a = steering_vector;
+    threshold = 1e-6;
+    maxIter = 30;
 %% Base Equations
-    % Size
-    N = max(length(x_vec));
-    % Identity matrix
-    I = eye(n);
+    [M, thetaNum] = size(a);
+    t_samples = size(x, 2);
+    R_N = (x*x')/t_samples;
     % First power value
-    P(1) = 1 / (A' * inv(diff(R)) * A); 
-    % Iteravtive Adaptive Approach (IAA) covariance matrix
-    sym x;
-    R = @x A * diag(P(x)) * A' + sigma^2 * I; 
-%% Power Value Iteration
-    while abs(P(x+1) - P(x)) < err
-        % SAMV0
-        P(x+1) = (A'*R*inv(P(x))*R*R*inv(P(x))*A) / (A'*R*inv(P(x))*A)^2;
-        % Update sigma
-        sigma = sqrt((R*inv(inv(P(x)))*R).' / (R*inv(inv(P(x)))).');
+    p_vec_Old = 1 / (a' * inv(diff(R_N)) * a);
+    sigma =  mean(abs(x(:)).^2);
+    % Iteration
+    for iterIdx = 1:maxIter
+        R =  a*spdiags(p_vec_Old, 0, thetaNum, thetaNum)*a' + sigma*eye(M);% 
+        Rinv=inv(R);
+        Rinv_RN_Rinv_A = Rinv * R_N * Rinv * a;
+        p_vec = p_vec_Old.^2 .* sum(conj(a).*Rinv_RN_Rinv_A, 1).';
+        sigma = real(trace(Rinv*Rinv*R_N))/real(trace(Rinv*Rinv));
+        % Check for convergents
+        if norm(p_vec_Old-p_vec)/norm(p_vec_Old)<threshold
+            break;
+        end
+        p_vec_Old = p_vec;
     end
+    p_vec = real(p_vec);
 %% Find Greatest Power
-    P_max = max(P,[],'all');
+    P_max = max(p_vec,[],'all');
     % Returns [Row,Col]
     [Az,El] = find(P_max == P);
 end
