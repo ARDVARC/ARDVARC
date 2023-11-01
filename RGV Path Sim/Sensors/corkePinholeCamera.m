aafba67358388a0499521d73ed134c0c4bf3b94b
Lyon Foster <lyon.foster@colorado.edu>
2023-11-01 14:53:01 -0600

    Function stub for camera model

---
+++ RGV Path Sim/Sensors/corkePinholeCamera.m
@@ -0,0 +1,11 @@
+function rendered_image = corkePinholeCamera(
+    uas2rgv_pos_exp_uasBodyFrame,
+    pinhole_location,
+    focal_length,
+
+    )
+%corkePinholeCamera See Corke Ch.11 for details 
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
