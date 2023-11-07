d968cf9c9b924b3aa45cce91962d22bbc84ff98d
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2023-11-07 14:10:32 -0700

    Add legend to 3D plotter

--- Orbiting Azimuth Sim/Plotters/plotOrbitFromData.m
+++ Orbiting Azimuth Sim/Plotters/plotOrbitFromData.m
@@ -11,14 +11,14 @@ function plotOrbitFromData(trix_vec_samplePosition_enu, trix_vec_truePointingVec
     close all
 
     figure(1)
-    scatter3(trix_vec_samplePosition_enu(:,1),trix_vec_samplePosition_enu(:,2),trix_vec_samplePosition_enu(:,3))
+    plots(1) = scatter3(trix_vec_samplePosition_enu(:,1),trix_vec_samplePosition_enu(:,2),trix_vec_samplePosition_enu(:,3),DisplayName="True UAS Locations");
     hold on
     grid minor
     axis equal
     % axis vis3d
     % setAxes3DPanAndZoomStyle(zoom(gca),gca,'camera')
-    scatter3(0,0,0,'g','filled','square')
-    scatter3(bestGuess(1),bestGuess(2),bestGuess(3),'r','filled','square')
+    plots(2) = scatter3(0,0,0,'g','filled','square',DisplayName="True RGV Location");
+    plots(3) = scatter3(bestGuess(1),bestGuess(2),bestGuess(3),'r','filled','square',DisplayName="Guessed RGV Location");
     if (zoomOnBottom)
         xlim([-params.orbitDistance/4 params.orbitDistance/4])
         ylim([-params.orbitDistance/4 params.orbitDistance/4])
@@ -29,8 +29,9 @@ function plotOrbitFromData(trix_vec_samplePosition_enu, trix_vec_truePointingVec
         zlim([0 params.height + 3*params.heightStd])
     end
 
-    plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu, 1, 'g');
-    plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu*2*params.height, 1, 'g:');
-    plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_sensorPointingVec_enu, 1, 'r');
-    plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_sensorPointingVec_enu*2*params.height, 1, 'r:');
+    plots(4) = plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu, 1, 'g', "True Poining Vectors");
+    plots(5) = plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu*2*params.height, 1, 'g:', "True Poining Line");
+    plots(6) = plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_sensorPointingVec_enu, 1, 'r', "Sensor Poining Vectors");
+    plots(7) = plot3DVectorsComingFrom(trix_vec_samplePosition_enu, trix_vec_sensorPointingVec_enu*2*params.height, 1, 'r:', "Sensor Poining Line");
+    legend(plots, Location="best")
 end
