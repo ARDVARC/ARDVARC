0960e38b3be54463dc8b253333f2e8a2e7567df4
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2023-11-28 17:42:23 -0700

    Make pinhole camera an option

--- Orbiting Azimuth Sim/getDataForParams.m
+++ Orbiting Azimuth Sim/getDataForParams.m
@@ -1,7 +1,7 @@
 function [trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu, trix_vec_sensorPointingVec_enu, vec_predictedRgvLocation_enu] = getDataForParams(params)
     % Runs a full orbit simulation based on params and returns important
     % data over the course of the simulation such as UAS states, pointing
-    % vectors, estimated position, and error
+    % vectors, and estimated position
     arguments(Input)
         params (1,1) OrbAzParams
     end
@@ -14,9 +14,12 @@ function [trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu, trix_vec_se
     
     [trix_vec_samplePosition_enu, trix_vec_truePointingVec_enu] = getTrueOrbitValues(params);
 
-    % [trix_vec_sensorPointingVec_enu] = getSensorPointingVecs(trix_vec_truePointingVec_enu, params);
-    trix_vec_uas2rgv_enu = -trix_vec_samplePosition_enu;
-    trix_vec_sensorPointingVec_enu = getPinholePointingVecs(trix_vec_uas2rgv_enu, params);
+    if (params.usePinholeCameraAsSensor)
+        trix_vec_uas2rgv_enu = -trix_vec_samplePosition_enu;
+        trix_vec_sensorPointingVec_enu = getPinholePointingVecs(trix_vec_uas2rgv_enu, params);
+    else
+        trix_vec_sensorPointingVec_enu = getSensorPointingVecs(trix_vec_truePointingVec_enu, params);
+    end
 
     vec_predictedRgvLocation_enu = getPrediction(trix_vec_sensorPointingVec_enu, trix_vec_samplePosition_enu, params);
 end
