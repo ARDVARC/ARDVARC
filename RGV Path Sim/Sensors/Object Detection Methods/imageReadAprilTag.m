435f5859905cc72236d522cbf78a475558d9e356
Tim-Behrer <147452551+Tim-Behrer@users.noreply.github.com>
2023-11-29 19:50:30 -0700

    April tag Video marking
    Add a video with april tags
    Add a script to read in an april tag
    Note - The AR tag family used is not accurate

---
+++ RGV Path Sim/Sensors/Object Detection Methods/imageReadAprilTag.m
@@ -0,0 +1,41 @@
+%% Read in April Tags
+% Setup: create Video Reader and Writer
+videoFileReader = VideoReader("DJI_0014_2_30_V - Copy - Trim.MP4");
+myVideo = VideoWriter('DJI_0014R_2_30_V');
+tagFamily = "tag16H5";
+depVideoPlayer = vision.DeployableVideoPlayer;
+open(myVideo);
+%% Detect faces in each frame
+while hasFrame(videoFileReader)
+
+	% read video frame
+	videoFrame = readFrame(videoFileReader);
+	% process frame
+    [id,loc,detectedFamily] = readAprilTag(videoFrame,tagFamily);
+    
+
+    for idx = 1:length(id)
+            % Display the ID and tag family
+            % disp("Detected Tag ID, Family: " + id(idx) + ", " ...
+            %     + detectedFamily(idx));
+     
+            % Insert markers to indicate the locations
+            markerRadius = 8;
+            numCorners = size(loc,1);
+            markerPosition = [loc(:,:,idx),repmat(markerRadius,numCorners,1)];
+            if id(idx) == 25
+                videoFrame = insertShape(videoFrame,"FilledCircle",markerPosition,ShapeColor="red",Opacity=1);
+            end
+    end
+
+    % % Display video frame to screen
+	% depVideoPlayer(videoFrame);
+    % Write frame to final video file
+	writeVideo(myVideo, videoFrame);
+	pause(1/videoFileReader.FrameRate);
+
+end
+close(myVideo)
+
+%% Archived Code
+%I = imread("aprilTag16H5_30.png");
