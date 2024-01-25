03ded1a25ca175e9220da68cf37dbcb9d30da58c
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2024-01-25 12:31:46 -0700

    temp

--- Python Simulator/controller.py
+++ Python Simulator/controller.py
@@ -6,34 +6,38 @@ import matplotlib.pyplot as plt
 import simulator
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.connect(("localhost", 1234))
-        # Move forward 5 seconds
-        s.sendall(bytearray([simulator.SimulatorMessageKind.simulateUntil]))
-        s.sendall(struct.pack("!d", 5))
-        ack = s.recv(1)
+        # Send reset command
+        s.sendall(bytearray([simulator.SimulatorMessageKind.reset]))
+        s.recv(1) # ACK
+        # # Move forward 5 seconds
+        # s.sendall(bytearray([simulator.SimulatorMessageKind.simulateUntil]))
+        # s.sendall(struct.pack("!d", 5))
+        # s.recv(1) # ACK
         
-        N = 160
+        N = 500
         trix_vec_uasPositions_local = np.empty((3,N))
         trix_vec_rgv1Predictions_local = np.empty((3,N))
-        i = 0
-        while i < N:
-            i += 1
+        for i in range(1,N+1):
             # Move to next time
             s.sendall(bytearray([simulator.SimulatorMessageKind.simulateUntil]))
-            s.sendall(struct.pack("!d", 5+i/16))
-            ack = s.recv(1)
+            s.sendall(struct.pack("!d", i/5))
+            s.recv(1) # ACK
+            
             # Measure RGV 1
             s.sendall(bytearray([simulator.SimulatorMessageKind.measureRgv1]))
             pointingBytes = s.recv(24)
             pointingVals = struct.unpack("!ddd", pointingBytes)
             vec_pointing_uas = np.array(pointingVals)
+            
             # Measure UAS
             s.sendall(bytearray([simulator.SimulatorMessageKind.measureUas]))
             uasStateBytes = s.recv(96)
             uasStateVals = struct.unpack("!dddddddddddd", uasStateBytes)
             _, _, z, phi, theta, psi, _, _, _, _, _, _ = uasStateVals
             vec_uasState = np.array(uasStateVals)
+            
             # Project to ground to get RGV prediction
             sinphi = np.sin(phi)
             cosphi = np.cos(phi)
@@ -47,12 +51,19 @@ def main():
                 [-sintheta      ,sinphi*costheta                     ,cosphi*costheta                     ]
             ])
             vec_pointingVec_enu = dcm_uas2local@vec_pointing_uas
-            # print(vec_pointingVec_enu)
             coeff = z/vec_pointingVec_enu[2]
+            vec_rgv1Prediction_local = vec_uasState[0:3] - vec_pointingVec_enu * coeff
             
-            trix_vec_rgv1Predictions_local[:,i-1] = vec_uasState[0:3] - vec_pointingVec_enu * coeff
+            # Set waypoint to above RGV measurement
+            s.sendall(bytearray([simulator.SimulatorMessageKind.setComplexWaypoint]))
+            s.sendall(struct.pack("!dddddd", vec_rgv1Prediction_local[0], vec_rgv1Prediction_local[1], 10, vec_rgv1Prediction_local[0], vec_rgv1Prediction_local[1], vec_rgv1Prediction_local[2]))
+            s.recv(1) # ACK
+            
+            # Store info to plot later
+            trix_vec_rgv1Predictions_local[:,i-1] = vec_rgv1Prediction_local
             trix_vec_uasPositions_local[:,i-1] = vec_uasState[0:3]
-    plt.scatter(trix_vec_rgv1Predictions_local[0,:],trix_vec_rgv1Predictions_local[1,:])
+    plt.scatter(trix_vec_rgv1Predictions_local[0,:],trix_vec_rgv1Predictions_local[1,:], s=1, c="red")
+    plt.scatter(trix_vec_uasPositions_local[0,:],trix_vec_uasPositions_local[1,:], s=1, c="green")
     plt.axis('equal')
     plt.grid(which='both')
     plt.show()
--- Python Simulator/simulator.py
+++ Python Simulator/simulator.py
@@ -21,44 +21,43 @@ def main():
     rgv1 = RGV.RGV(1, np.array([5,5]), np.pi/2, 900)
     rgv2 = RGV.RGV(2, np.array([-5,-5]), -np.pi/2, 900)
     tPrev = 0
-    vec_uasStatePrev = np.array([-30,0,0,0,0,0,0,0,0,0,0,0])
+    vec_uasStatePrev = np.array([-30,0,10,0,0,0,0,0,0,0,0,0])
     complexWaypoint = uasPhysics.ComplexWaypoint(np.array([0,0,10]),np.array([0,0,0]))
     
     # Run simulation as commanded
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.bind(("localhost", 1234))
-        s.listen()
-        connection, _ = s.accept()
-        with connection:
+        with s:
             while True:
-                messageKind = SimulatorMessageKind(connection.recv(1)[0])
+                data, address = s.recvfrom(1)
+                messageKind = SimulatorMessageKind(data[0])
                 # print(messageKind)
                 match messageKind:
                     case SimulatorMessageKind.simulateUntil:
-                        t: float = struct.unpack("!d", connection.recv(8))[0]
+                        t: float = struct.unpack("!d", s.recv(8))[0]
                         if t <= tPrev:
                             raise Exception(f"Asked to simulate until invalid time {t} (tPrev: {tPrev}).")
                         sol = uasPhysics.simulateUasPhysics((tPrev, t), vec_uasStatePrev, complexWaypoint)
                         vec_uasStatePrev = sol.y[:,-1]
                         tPrev = t
-                        connection.send(bytearray([6]))
+                        s.sendto(bytearray([6]), address)
                     case SimulatorMessageKind.measureRgv1:
                         vec_sensorPointingVec_uas = measureRgv(rgv1, tPrev, vec_uasStatePrev)
-                        connection.send(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]))
+                        s.sendto(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]), address)
                     case SimulatorMessageKind.measureRgv2:
                         vec_sensorPointingVec_uas = measureRgv(rgv2, tPrev, vec_uasStatePrev)
-                        connection.send(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]))
+                        s.sendto(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]), address)
                     case SimulatorMessageKind.measureUas:
-                        connection.send(struct.pack("!dddddddddddd", vec_uasStatePrev[0], vec_uasStatePrev[1], vec_uasStatePrev[2], vec_uasStatePrev[3], vec_uasStatePrev[4], vec_uasStatePrev[5], vec_uasStatePrev[6], vec_uasStatePrev[7], vec_uasStatePrev[8], vec_uasStatePrev[9], vec_uasStatePrev[10], vec_uasStatePrev[11]))
+                        s.sendto(struct.pack("!dddddddddddd", vec_uasStatePrev[0], vec_uasStatePrev[1], vec_uasStatePrev[2], vec_uasStatePrev[3], vec_uasStatePrev[4], vec_uasStatePrev[5], vec_uasStatePrev[6], vec_uasStatePrev[7], vec_uasStatePrev[8], vec_uasStatePrev[9], vec_uasStatePrev[10], vec_uasStatePrev[11]), address)
                     case SimulatorMessageKind.setComplexWaypoint:
-                        vals = struct.unpack("!dddddd", connection.recv(48))
+                        vals = struct.unpack("!dddddd", s.recv(48))
                         complexWaypoint = uasPhysics.ComplexWaypoint(np.array([vals[0],vals[1],vals[2]]), np.array([vals[3],vals[4],vals[5]]))
-                        connection.send(bytearray([6]))
+                        s.sendto(bytearray([6]), address)
                     case SimulatorMessageKind.reset:
                         tPrev = 0
-                        vec_uasStatePrev = np.array([-30,0,0,0,0,0,0,0,0,0,0,0])
+                        vec_uasStatePrev = np.array([-30,0,10,0,0,0,0,0,0,0,0,0])
                         complexWaypoint = uasPhysics.ComplexWaypoint(np.array([0,0,10]),np.array([0,0,0]))
-                        connection.send(bytearray([6]))
+                        s.sendto(bytearray([6]), address)
 
 def measureRgv(rgv: RGV.RGV, tPrev: float, vec_uasState: np.ndarray) -> np.ndarray:
     vec_uasPos_local = vec_uasState[0:3]
@@ -99,7 +98,7 @@ def measureRgv(rgv: RGV.RGV, tPrev: float, vec_uasState: np.ndarray) -> np.ndarr
     # and the angle is random based on the pointing angle standard
     # deviation parameter. Then generate the rotation matrix corresponding
     # to the specified axis and angle.
-    dcm_truePointingVec2sensorPointingVec = R.from_rotvec(vec_randomOrthogonal_local * (random.normalvariate(0,np.deg2rad(5))+2*np.pi)).as_matrix()
+    dcm_truePointingVec2sensorPointingVec = R.from_rotvec(vec_randomOrthogonal_local * (random.normalvariate(0,np.deg2rad(20))+2*np.pi)).as_matrix()
     # Rotate the true pointing vector by the pointing
     # error rotation to get the sensor pointing vector.
     vec_sensorPointingVec_local = dcm_truePointingVec2sensorPointingVec@vec_pointingVecToRgv_local
