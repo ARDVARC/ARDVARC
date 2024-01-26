059675739bceef79fabf3d61d88b924db4c9086c
Aidan Wood <82551807+aidanwoodcu@users.noreply.github.com>
2024-01-26 14:38:30 -0700

    Add basic automatic messages

--- Python Simulator/controller.py
+++ Python Simulator/controller.py
@@ -1,60 +1,34 @@
 import asyncio
 import socket
 import struct
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation as R 
 
 import simulator
+import mavgen_common as mg
 
 async def main():
     reader, writer = await asyncio.open_connection("localhost", 1234)
-    # Send reset command
-    writer.write(bytearray([simulator.SimulatorMessageKind.reset]))
-    await reader.read(1) # ACK
-    # # Move forward 5 seconds
-    # s.sendall(bytearray([simulator.SimulatorMessageKind.simulateUntil]))
-    # s.sendall(struct.pack("!d", 5))
-    # s.recv(1) # ACK
-    
-    N = 500
-    trix_vec_uasPositions_local_ned = np.empty((3,N))
-    trix_vec_rgv1Predictions_local_ned = np.empty((3,N))
-    for i in range(1,N+1):
-        # Move to next time
-        writer.write(bytearray([simulator.SimulatorMessageKind.simulateUntil]))
-        writer.write(struct.pack("!d", i/5))
-        await reader.read(1) # ACK
-        
-        # Measure RGV 1
-        writer.write(bytearray([simulator.SimulatorMessageKind.measureRgv1]))
-        pointingBytes = await reader.read(24)
-        pointingVals = struct.unpack("!ddd", pointingBytes)
-        vec_pointing_uas = np.array(pointingVals)
-        
-        # Measure UAS
-        writer.write(bytearray([simulator.SimulatorMessageKind.measureUas]))
-        uasStateBytes = await reader.read(96)
-        uasStateVals = struct.unpack("!dddddddddddd", uasStateBytes)
-        vec_uasState = np.array(uasStateVals)
-        print(vec_uasState[0:3])
-        
-        # Project to ground to get RGV prediction
-        dcm_uas2local_ned = R.from_euler("xyz", vec_uasState[3:6]).as_matrix()
-        vec_pointingVec_local_ned = dcm_uas2local_ned@vec_pointing_uas
-        coeff = uasStateVals[2]/vec_pointingVec_local_ned[2]
-        vec_rgv1Prediction_local_ned = vec_uasState[0:3] - vec_pointingVec_local_ned * coeff
-        
-        # Set waypoint to above RGV measurement
-        writer.write(bytearray([simulator.SimulatorMessageKind.setComplexWaypoint]))
-        writer.write(struct.pack("!dddddd", vec_rgv1Prediction_local_ned[0], vec_rgv1Prediction_local_ned[1], -10, vec_rgv1Prediction_local_ned[0], vec_rgv1Prediction_local_ned[1], vec_rgv1Prediction_local_ned[2]))
-        await reader.read(1) # ACK
-        
-        # Store info to plot later
-        trix_vec_rgv1Predictions_local_ned[:,i-1] = vec_rgv1Prediction_local_ned
-        trix_vec_uasPositions_local_ned[:,i-1] = vec_uasState[0:3]
-    plt.scatter(trix_vec_rgv1Predictions_local_ned[1,:],trix_vec_rgv1Predictions_local_ned[0,:], s=1, c="red")
-    plt.scatter(trix_vec_uasPositions_local_ned[1,:],trix_vec_uasPositions_local_ned[0,:], s=1, c="green")
+    mv = mg.MAVLink(writer, 1, mg.MAV_COMP_ID_ONBOARD_COMPUTER)
+    # Send requests 
+    mv.command_long_send(1, mg.MAV_COMP_ID_AUTOPILOT1, mg.MAV_CMD_SET_MESSAGE_INTERVAL, 0, mg.MAVLINK_MSG_ID_LOCAL_POSITION_NED, 1e6, 0, 0, 0, 0, 0)
+    mv.command_long_send(1, mg.MAV_COMP_ID_AUTOPILOT1, mg.MAV_CMD_SET_MESSAGE_INTERVAL, 0, mg.MAVLINK_MSG_ID_ATTITUDE, 1e6, 0, 0, 0, 0, 0)
+    xs = []
+    ys = []
+    timer = asyncio.create_task(asyncio.sleep(30))
+    while not timer.done():
+        buffer = await reader.read(sys.maxsize)
+        messages = mv.parse_buffer(buffer)
+        if messages == None:
+            continue
+        for msg in messages:
+            match msg:
+                case mg.MAVLink_local_position_ned_message():
+                    xs.append(msg.x)
+                    ys.append(msg.y)
+    plt.scatter(xs, ys, s=1, c="green")
     plt.axis('equal')
     plt.grid(which='both')
     plt.show()
--- Python Simulator/simulator.py
+++ Python Simulator/simulator.py
@@ -2,13 +2,16 @@ from enum import IntEnum
 import random
 import socket
 import struct
+import sys
+import time
+from typing import NoReturn
 import numpy as np
 from scipy.spatial.transform import Rotation as R 
 import asyncio
-import mavgen_common as mg
 
 import rgv as RGV
 import uasPhysics
+import mavgen_common as mg
 
 class SimulatorMessageKind(IntEnum):
     simulateUntil = 0
@@ -18,54 +21,54 @@ class SimulatorMessageKind(IntEnum):
     setComplexWaypoint = 4
     reset = 5
 
+rgv1 = RGV.RGV(1, np.array([5,5]), np.pi/2, 900)
+rgv2 = RGV.RGV(2, np.array([-5,-5]), -np.pi/2, 900)
+global_lock = asyncio.Lock()
+start_time = time.time()
+def timeSinceStart() -> float:
+    return time.time() - start_time
+tPrev = 0
+vec_uasStatePrev = np.array([-30,0,-10,0,0,0,0,0,0,0,0,0])
+dcm_local_ned2uas = np.zeros((3,3))
+complexWaypoint = uasPhysics.ComplexWaypoint(np.array([0,0,-10]),np.array([0,0,0]))
+
 async def main():
-    # Run simulation as commanded
+    asyncio.create_task(physicsLoop())
     server = await asyncio.start_server(handle_client, "localhost", 1234)
     async with server:
         await server.serve_forever()
 
 async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
-    # Initialize simulation
-    rgv1 = RGV.RGV(1, np.array([5,5]), np.pi/2, 900)
-    rgv2 = RGV.RGV(2, np.array([-5,-5]), -np.pi/2, 900)
-    tPrev = 0
-    vec_uasStatePrev = np.array([-30,0,-10,0,0,0,0,0,0,0,0,0])
-    dcm_local_ned2uas = np.zeros((3,3))
-    complexWaypoint = uasPhysics.ComplexWaypoint(np.array([0,0,10]),np.array([0,0,0]))
-    
+    mv = mg.MAVLink(writer, 1, mg.MAV_COMP_ID_AUTOPILOT1)
+    temp = asyncio.create_task(heartbeatLoop(mv))
+    message_loops = dict[int,asyncio.Task[NoReturn]]()
     while True:
-        data = await reader.read(1)
-        messageKind = SimulatorMessageKind(data[0])
-        # print(messageKind)
-        match messageKind:
-            case SimulatorMessageKind.simulateUntil:
-                t: float = struct.unpack("!d", await reader.read(8))[0]
-                if t <= tPrev:
-                    raise Exception(f"Asked to simulate until invalid time {t} (tPrev: {tPrev}).")
-                sol = uasPhysics.simulateUasPhysics((tPrev, t), vec_uasStatePrev, complexWaypoint)
-                tPrev = t
-                vec_uasStatePrev = sol.y[:,-1]
-                dcm_local_ned2uas = R.from_euler("xyz", vec_uasStatePrev[3:6]).as_matrix().transpose()
-                
-                writer.write(bytearray([6]))
-            case SimulatorMessageKind.measureRgv1:
-                vec_sensorPointingVec_uas = measureRgv(rgv1, tPrev, vec_uasStatePrev, dcm_local_ned2uas)
-                writer.write(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]))
-            case SimulatorMessageKind.measureRgv2:
-                vec_sensorPointingVec_uas = measureRgv(rgv2, tPrev, vec_uasStatePrev, dcm_local_ned2uas)
-                writer.write(struct.pack("!ddd", vec_sensorPointingVec_uas[0], vec_sensorPointingVec_uas[1], vec_sensorPointingVec_uas[2]))
-            case SimulatorMessageKind.measureUas:
-                # mg.MAVLink.local_position_ned_encode(self, tPrev, )
-                writer.write(struct.pack("!dddddddddddd", vec_uasStatePrev[0], vec_uasStatePrev[1], vec_uasStatePrev[2], vec_uasStatePrev[3], vec_uasStatePrev[4], vec_uasStatePrev[5], vec_uasStatePrev[6], vec_uasStatePrev[7], vec_uasStatePrev[8], vec_uasStatePrev[9], vec_uasStatePrev[10], vec_uasStatePrev[11]))
-            case SimulatorMessageKind.setComplexWaypoint:
-                vals = struct.unpack("!dddddd", await reader.read(48))
-                complexWaypoint = uasPhysics.ComplexWaypoint(np.array([vals[0],vals[1],vals[2]]), np.array([vals[3],vals[4],vals[5]]))
-                writer.write(bytearray([6]))
-            case SimulatorMessageKind.reset:
-                tPrev = 0
-                vec_uasStatePrev = np.array([-30,0,-10,0,0,0,0,0,0,0,0,0])
-                complexWaypoint = uasPhysics.ComplexWaypoint(np.array([0,0,10]),np.array([0,0,0]))
-                writer.write(bytearray([6]))
+        buffer = await reader.read(sys.maxsize)
+        messages = mv.parse_buffer(buffer)
+        if messages == None:
+            continue
+        for msg in messages:
+            match msg:
+                case mg.MAVLink_command_long_message():
+                    match msg.command:
+                        case mg.MAV_CMD_SET_MESSAGE_INTERVAL:
+                            messageId = msg.param1
+                            interval = msg.param2
+                            match messageId:
+                                case mg.MAVLINK_MSG_ID_LOCAL_POSITION_NED | mg.MAVLINK_MSG_ID_ATTITUDE:
+                                    if messageId in message_loops:
+                                        message_loops[messageId].cancel()
+                                    match messageId:
+                                        case mg.MAVLINK_MSG_ID_LOCAL_POSITION_NED:
+                                            message_loops[messageId] = asyncio.create_task(localPositionLoop(mv, interval))
+                                        case mg.MAVLINK_MSG_ID_ATTITUDE:
+                                            message_loops[messageId] = asyncio.create_task(attitudeLoop(mv, interval))
+                                case other:
+                                    print(f"Simulator cannot set message interval for unrecognized message ID {other}")
+                        case other:
+                            print(f"Simulator does not recognize command ID {other}")
+                case other:
+                    print(f"Simulator does not recognize message {type(other)}")
 
 def measureRgv(rgv: RGV.RGV, tPrev: float, vec_uasState: np.ndarray, dcm_local_ned2uas: np.ndarray) -> np.ndarray:
     _, vec_rgvPosition_local_ne = RGV.getRgvStateAtTime(rgv, tPrev)
@@ -94,6 +97,59 @@ def measureRgv(rgv: RGV.RGV, tPrev: float, vec_uasState: np.ndarray, dcm_local_n
     # Finally convert to the UAS frame
     vec_sensorPointingVec_uas = dcm_local_ned2uas@vec_sensorPointingVec_local_ned
     return vec_sensorPointingVec_uas
+
+async def physicsLoop():
+    global global_lock, tPrev, vec_uasStatePrev, dcm_local_ned2uas, complexWaypoint
+    DT = 1/(2**8)
+    while True:
+        sleeper = asyncio.create_task(asyncio.sleep(tPrev - timeSinceStart() + DT))
+        sol = uasPhysics.simulateUasPhysics((tPrev, tPrev + DT), vec_uasStatePrev, complexWaypoint)
+        temp_dcm_local_ned2uas = R.from_euler("xyz", vec_uasStatePrev[3:6]).as_matrix().transpose()
+        if sleeper.done():
+            print("WARNING: Simulation is going slower than real time")
+        await sleeper
+        async with global_lock:
+            tPrev += DT
+            vec_uasStatePrev = sol.y[:,-1]
+            dcm_local_ned2uas = temp_dcm_local_ned2uas
+
+async def heartbeatLoop(mv: mg.MAVLink):
+    heartbeat_last_sent = timeSinceStart()
+    DT = 1
+    while True:
+        await asyncio.create_task(asyncio.sleep(heartbeat_last_sent - timeSinceStart() + DT))
+        mv.heartbeat_send(mg.MAV_TYPE_HEXAROTOR, mg.MAV_AUTOPILOT_GENERIC, mg.MAV_MODE_FLAG_HIL_ENABLED, 0, mg.MAV_STATE_ACTIVE)
+        heartbeat_last_sent += DT
+
+async def localPositionLoop(mv: mg.MAVLink, interval: float):
+    global global_lock, tPrev, vec_uasStatePrev, dcm_local_ned2uas
+    local_position_last_sent = timeSinceStart()
+    interval /= 1e6
+    while True:
+        await asyncio.create_task(asyncio.sleep(local_position_last_sent - timeSinceStart() + interval))
+        async with global_lock:
+            t = tPrev
+            vec_uasStatePrev_copy = vec_uasStatePrev.copy()
+            dcm_local_ned2uas_copy = dcm_local_ned2uas.copy()
+        dcm_uas2local_ned = dcm_local_ned2uas_copy.transpose()
+        vec_vel_local_ned = dcm_uas2local_ned.transpose()@vec_uasStatePrev_copy[6:9]
+        mv.local_position_ned_send(int(t*1e3), vec_uasStatePrev_copy[0], vec_uasStatePrev_copy[1], vec_uasStatePrev_copy[2], vec_vel_local_ned[0], vec_vel_local_ned[1], vec_vel_local_ned[2])
+        local_position_last_sent += interval
+
+async def attitudeLoop(mv: mg.MAVLink, interval: float):
+    global global_lock, tPrev, vec_uasStatePrev, dcm_local_ned2uas
+    attitude_last_sent = timeSinceStart()
+    interval /= 1e6
+    while True:
+        await asyncio.create_task(asyncio.sleep(attitude_last_sent - timeSinceStart() + interval))
+        async with global_lock:
+            t = tPrev
+            vec_uasStatePrev_copy = vec_uasStatePrev.copy()
+            dcm_local_ned2uas_copy = dcm_local_ned2uas.copy()
+        dcm_uas2local_ned = dcm_local_ned2uas_copy.transpose()
+        vec_ang_vel_local_ned = dcm_uas2local_ned.transpose()@vec_uasStatePrev_copy[9:12]
+        mv.attitude_send(int(t*1e3), vec_uasStatePrev_copy[3], vec_uasStatePrev_copy[4], vec_uasStatePrev_copy[5], vec_ang_vel_local_ned[0], vec_ang_vel_local_ned[1], vec_ang_vel_local_ned[2])
+        attitude_last_sent += interval
 
 if __name__ == "__main__":
     asyncio.run(main())
