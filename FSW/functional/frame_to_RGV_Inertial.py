31e710b0d5fcf11c9a59569faf99d7281db80d88
Tim-Behrer <147452551+Tim-Behrer@users.noreply.github.com>
2024-01-31 21:44:57 -0700

    Create frame_to_RGV_Inertial.py

---
+++ FSW/functional/frame_to_RGV_Inertial.py
@@ -0,0 +1,24 @@
+"""
+ARDVARC Flight Software
+
+Author: Timothy Behrer
+email: tibe0349@colorado.edu
+
+Description:
+    File for the camera vision and RGV inertial position estimate
+
+Notes:
+    TODO Implement camera vision object recognition software
+    TODO Implement camera (in/ex)trinsics
+    TODO Implement camera variance
+    TODO Implement frame transition and UAS state to backout the RGV inertial position
+
+"""
+
+def frame_to_rgv_inert(frame):
+    
+    pass
+
+
+
+
