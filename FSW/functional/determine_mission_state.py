8fb99bff059d5243600be40647073a0406f64549
Aidan Wood <aiwo9411@colorado.edu>
2024-02-12 15:06:48 -0700

    Make determine_mission_state.py MVP

--- FSW/functional/determine_mission_state.py
+++ FSW/functional/determine_mission_state.py
@@ -8,17 +8,21 @@ _current_state: MissionStates = MissionStates.FIND_RGV_1
 
 
 def _state_machine_criteria_callback(msg: StateMachineCriteria):
+    """
+    This is the callback used by the state machine criteria subscriber. It uses the new criteria along with
+    the known current state in order to determine the next state, which it then publishes.
+    """
+    
+    global _current_state
     # Choose the next mission state based on _current_state and msg
-    # new_state = math math math
-    # _current_state = new_state
+    _current_state = determine_next_mission_state(_current_state, msg)
     
     # Publish new mission state
-    _mission_state_pub.publish(
-        MissionState(
-            # TODO: Make this something reasonable
-        )
-    )
-    rospy.loginfo("Misison state published")
+    mission_state_msg = MissionState()
+    mission_state_msg.timestamp = msg.timestamp
+    mission_state_msg.mission_state = _current_state.value
+    _mission_state_pub.publish(mission_state_msg)
+    rospy.loginfo("Mission state published")
 
 
 def setup():
@@ -33,3 +37,94 @@ def setup():
 
 _mission_state_pub: rospy.Publisher
 _state_machine_criteria_sub: rospy.Subscriber
+
+
+def determine_next_mission_state(current_state: MissionStates, criteria: StateMachineCriteria) -> MissionStates:
+    """
+    Determines what the next mission state should be based on the current mission state and a set of
+    state machine criteria. This is meant to identically implement the state machine design in the
+    Software Function Tracker spreadsheet.
+    """
+    
+    # Check for GO_HOME first because it always behaves the same
+    if current_state is MissionStates.GO_HOME or criteria.battery_low:
+        return MissionStates.GO_HOME
+    
+    if current_state is MissionStates.FIND_RGV_1:
+        if criteria.recent_rgv_1_estimate:
+            # Change to TRACK_RGV_1
+            return MissionStates.TRACK_RGV_1
+        else:
+            # Stay in FIND_RGV_1
+            return MissionStates.FIND_RGV_1
+    elif current_state is MissionStates.TRACK_RGV_1:
+        if ~criteria.recent_rgv_1_estimate:
+            # Change to FIND_RGV_1
+            return MissionStates.FIND_RGV_1
+        elif criteria.rgv_1_is_moving or ~criteria.rgv_1_sighted:
+            # Stay in TRACK_RGV_1
+            return MissionStates.TRACK_RGV_1
+        else:
+            # Change to LOCALIZE_RGV_1
+            return MissionStates.LOCALIZE_RGV_1
+    elif current_state is MissionStates.LOCALIZE_RGV_1:
+        if ~criteria.recent_rgv_1_estimate:
+            # Change to FIND_RGV_1
+            return MissionStates.FIND_RGV_1
+        if criteria.rgv_1_localized:
+            # Change to RGV 2
+            if criteria.recent_rgv_2_estimate:
+                # Change to TRACK_RGV_2
+                return MissionStates.TRACK_RGV_2
+            else:
+                # Change to FIND_RGV_2
+                return MissionStates.FIND_RGV_2
+        else:
+            # Stay on RGV 1
+            if (criteria.rgv_1_is_moving and ~criteria.minimum_localize_time_reached) or ~criteria.rgv_1_sighted:
+                # Change to TRACK_RGV_1
+                return MissionStates.TRACK_RGV_1
+            else:
+                # Stay in LOCALIZE_RGV_1
+                return MissionStates.LOCALIZE_RGV_1
+    elif current_state is MissionStates.FIND_RGV_2:
+        if criteria.recent_rgv_2_estimate:
+            # Change to TRACK_RGV_2
+            return MissionStates.TRACK_RGV_2
+        else:
+            # Stay in FIND_RGV_2
+            return MissionStates.FIND_RGV_2
+    elif current_state is MissionStates.TRACK_RGV_2:
+        if ~criteria.recent_rgv_2_estimate:
+            # Change to FIND_RGV_2
+            return MissionStates.FIND_RGV_2
+        elif criteria.rgv_2_is_moving or ~criteria.rgv_2_sighted:
+            # Stay in TRACK_RGV_2
+            return MissionStates.TRACK_RGV_2
+        else:
+            # Change to LOCALIZE_RGV_2
+            return MissionStates.LOCALIZE_RGV_2
+    elif current_state is MissionStates.LOCALIZE_RGV_2:
+        if ~criteria.recent_rgv_2_estimate:
+            # Change to FIND_RGV_2
+            return MissionStates.FIND_RGV_2
+        if criteria.rgv_2_localized:
+            # Change to JOINT_LOCALIZE
+            return MissionStates.JOINT_LOCALIZE
+        else:
+            # Stay on RGV 2
+            if (criteria.rgv_2_is_moving and ~criteria.minimum_localize_time_reached) or ~criteria.rgv_2_sighted:
+                # Change to TRACK_RGV_2
+                return MissionStates.TRACK_RGV_2
+            else:
+                # Stay in LOCALIZE_RGV_2
+                return MissionStates.LOCALIZE_RGV_2
+    elif current_state is MissionStates.JOINT_LOCALIZE:
+        if criteria.joint_localized:
+            # Restart mission state cycle
+            return MissionStates.FIND_RGV_1
+        else:
+            # Stay in JOINT_LOCALIZE
+            return MissionStates.JOINT_LOCALIZE
+    
+    raise Exception("determine_next_mission_state did not return")
