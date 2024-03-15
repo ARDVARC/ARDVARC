286c7d8d0c3b15e91eb886991ea5eeac550076b2
Aidan Wood <aiwo9411@colorado.edu>
2024-03-15 11:59:10 -0600

    Add limited bluetooth support to black_magic.sh
    We should really move all FSW to rosardvarc so that we can launch with roslaunch

--- FSW/black_magic.sh
+++ FSW/black_magic.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 cd ..
+
+if [ "$1" == "read_raw_bluetooth" ]
+then
+    echo "Reading raw bluetooth"
+    python3 -m FSW.functional.read_raw_bluetooth_from_array &
+fi
+
 python3 -m FSW.cv &
 python3 -m FSW.main
