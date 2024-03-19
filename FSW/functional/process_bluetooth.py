4920f968f05cd0932195826c9a07bc9cd9755608
John Davis <132501461+joda2486@users.noreply.github.com>
2024-03-18 18:48:39 -0600

    WIP bluetooth processing
    started bluetooth processing

--- FSW/functional/process_bluetooth.py
+++ FSW/functional/process_bluetooth.py
@@ -1,18 +1,40 @@
 import rospy
 from rosardvarc.msg import UasToRgvDirectionVectorUasFrame, BluetoothAzimuthElevation
 from ..config.topic_names import UAS_TO_RGV_DIRECTION_VECTORS, RAW_BLUETOOTH
+from ..config.constants import BLUETOOTH_DCM
+
+import math
+import numpy as np 
 
 
 def _bluetooth_callback(msg: BluetoothAzimuthElevation):
-    # math math math
+    # Calculates pointing vector in Bluetooth body frame
+    x = math.cos(BluetoothAzimuthElevation[2]*math.pi/180)*math.cos(BluetoothAzimuthElevation[1]*math.pi/180)
+    y = math.cos(BluetoothAzimuthElevation[2]*math.pi/180)*math.cos(BluetoothAzimuthElevation[1]*math.pi/180)
+    z = math.sin(BluetoothAzimuthElevation[2]*math.pi/180)
+
+    #TODO: convert calculated points to pointing vector (NEED Current attitude of BlueTooth Array in reference to UAS frame)
+
+    #TODO: Create storage array that stores values (Might not need if Bluetooth frequency is 20)
+
+    BluetoothToRgvDirectionVector = [[x,y,z]]
+
+    # Rotation Matrix Call goes here Euler Angles = [180,90,0]
+
+    UasToRgvDirectionVectorUasFrame = BluetoothToRgvDirectionVector
     
     rospy.logdebug("Bluetooth processor received raw bluetooth data")
     rospy.logdebug("Bluetooth processor published a direction vector")
-    _direction_vector_pub.publish(
+    
+    _direction_vector_pub.publish(UasToRgvDirectionVectorUasFrame)
+    
+    # publisher below seems much, unless someone had a syntax they wanted
+    '''_direction_vector_pub.publish(
         UasToRgvDirectionVectorUasFrame(
             # TODO: Make this something reasonable
+
         )
-    )
+    )'''
 
 
 def setup():
