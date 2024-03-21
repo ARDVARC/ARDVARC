f3e15fea4beca4fdcd0018a88f5f6d752b42338e
Aidan Wood <aiwo9411@colorado.edu>
2024-03-20 21:43:55 -0600

    Make calibration work with screenshots

--- RGV Path Sim/Sensors/Pi_cam_Calibration_code.py
+++ RGV Path Sim/Sensors/Pi_cam_Calibration_code.py
@@ -6,59 +6,36 @@ import cv2.aruco as aruco
 def calibrate_cam():
     # Define the dictionary
     aruco_dict = cv2.aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
-    board = cv2.aruco.CharucoBoard((8, 11), .015, .011, aruco_dict)
+    board = cv2.aruco.CharucoBoard((11, 8), .015, .011, aruco_dict)
     board.setLegacyPattern(True)
     params = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(aruco_dict, params)
-    charucodetector = cv2.aruco.CharucoDetector(board)
-
-
 
     # Arrays to store object points and image points from all images
     all_corners = []
     all_ids = []
-    decimator = 0
-    images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg", "img10.jpg"
-            , "img11.jpg", "img12.jpg", "img13.jpg", "img14.jpg"]
-
+    images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg", "img10.jpg",
+              "img11.jpg", "img12.jpg", "img13.jpg", "img14.jpg", "ss.jpg", "phone.jpg"]
+    # TODO: The list of images above should only contain pictures from the same camera, so remove ss.jpg and phone.jpg
 
     # Loop over your images
     for img in images:
         image = cv2.imread(img)
-        image_copy = image.copy()
-        # cv2.imshow('CharUco Board', image_copy)
-        # cv2.waitKey(0)
-        
 
         (corners, ids, _) = detector.detectMarkers(image)
-        # print(f"corners: {len(corners)}, ids: {ids}") ## Corners are being detected
 
         if len(corners) > 0:
-            cv2.aruco.drawDetectedMarkers(image_copy, corners, ids)
-            # cv2.imshow('CharUco Board', image_copy)
-            # cv2.waitKey(0)
-            ##All Ids are being detected
-
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            # cv2.imshow('CharUco Board', image)
-            # cv2.waitKey(0)
-            # import ipdb; ipdb.set_trace()
-            charuco_corners, charuco_ids, marker_corners, marker_ids = charucodetector.detectBoard(gray) #This is where the detector is breaking down
-            charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(marker_corners, marker_ids, image, board)
-            # board.matchImagePoints(charuco_corners, charuco_ids, )
-            cv2.aruco.drawDetectedMarkers(image_copy, marker_corners, marker_ids)
-            # cv2.aruco.drawDetectedMarkers(image_copy, charuco_corners, charuco_ids)
+            charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, image, board)
             
-            # cv2.imshow('CharUco Board', image_copy)
-            # cv2.waitKey(0)
-            import ipdb; ipdb.set_trace()
-            # print(f"charuco_corners: {len(charuco_corners)}")
-            all_corners.append(charuco_corners)
-            all_ids.append(charuco_ids)
+            if charuco_retval:
+                print("Interpolated!")
+                all_corners.append(charuco_corners)
+                all_ids.append(charuco_ids)
+            else:
+                print("Failed to interpolate")
     
     # Calibrate the camera
 
-    # import ipdb; ipdb.set_trace()
     retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_corners, all_ids, board, image.shape[:2], None, None)
 
     # Save calibration data
