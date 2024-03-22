0a8d53a0ddeee420771e2a00d6cd815b597734d6
Tim-Behrer <tibe0349@colorado.edu>
2024-03-21 19:20:55 -0600

    Got it to work!

--- RGV Path Sim/Sensors/Pi_cam_Calibration_code.py
+++ RGV Path Sim/Sensors/Pi_cam_Calibration_code.py
@@ -5,8 +5,9 @@ import cv2.aruco as aruco
 
 def calibrate_cam():
     # Define the dictionary
-    aruco_dict = cv2.aruco.getPredefinedDictionary(aruco.DICT_6X6_1000)
+    aruco_dict = cv2.aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
     board = cv2.aruco.CharucoBoard((11, 8), .015, .011, aruco_dict)
+    
     board.setLegacyPattern(True)
     params = cv2.aruco.DetectorParameters()
     detector = cv2.aruco.ArucoDetector(aruco_dict, params)
@@ -15,7 +16,7 @@ def calibrate_cam():
     all_corners = []
     all_ids = []
     images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg", "img6.jpg", "img7.jpg", "img8.jpg", "img9.jpg", "img10.jpg",
-              "img11.jpg", "img12.jpg", "img13.jpg", "img14.jpg", "ss.jpg", "phone.jpg"]
+              "img11.jpg", "img12.jpg", "img13.jpg", "img14.jpg"]# "ss.jpg", "phone.jpg","phone_print.jpg"]
     # TODO: The list of images above should only contain pictures from the same camera, so remove ss.jpg and phone.jpg
 
     # Loop over your images
@@ -23,6 +24,7 @@ def calibrate_cam():
         image = cv2.imread(img)
 
         (corners, ids, _) = detector.detectMarkers(image)
+        ids -= np.min(ids) #Correct for possible id offsets
 
         if len(corners) > 0:
             charuco_retval, charuco_corners, charuco_ids = cv2.aruco.interpolateCornersCharuco(corners, ids, image, board)
@@ -42,8 +44,9 @@ def calibrate_cam():
 
     retval, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.aruco.calibrateCameraCharuco(all_corners, all_ids, board, image.shape[:2], None, None)
     if camera_matrix is not None:
-        print("Calibration successful!")
-        import ipdb; ipdb.set_trace()
+        print("Calibration successful!\n")
+        print("Camera Matrix:\n", camera_matrix)
+        print("Distortion Coefficients:\n", dist_coeffs)
     # Save calibration data
     np.save('camera_matrix.npy', camera_matrix)
     np.save('dist_coeffs.npy', dist_coeffs)
